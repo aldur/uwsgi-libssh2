4c9d82edef2cbc00d51b3320cf6a7cd51f92806d
Aldur <adrianodl@hotmail.it>
2014-09-25 11:06:05 +0200

    improve nonblocking behavior; do not allocate memory but serve directly; overall improvements

--- libssh2.c
+++ libssh2.c
@@ -1,288 +1,269 @@
 #include <uwsgi.h>
+extern struct uwsgi_server uwsgi;
+
 #include <libssh2.h>
 #include <libssh2_sftp.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 
-static int waitsocket(int socket_fd, LIBSSH2_SESSION *session)
+#define BUFFER_SIZE 1024
+#define USER "vagrant"
+#define PASSWORD "vagrant"
+
+
+static void waitsocket(int socket_fd, LIBSSH2_SESSION *session)
 {
-    struct timeval timeout;
-    int rc;
-    fd_set fd;
-    fd_set *writefd = NULL;
-    fd_set *readfd = NULL;
-    int dir;
+	int dir = libssh2_session_block_directions(session);
 
-    timeout.tv_sec = 10;
-    timeout.tv_usec = 0;
+	if (dir & LIBSSH2_SESSION_BLOCK_INBOUND) {
+		if (uwsgi.wait_read_hook(socket_fd, uwsgi.socket_timeout) < 0) {
+			uwsgi_error("waitsocket()/wait_read_hook()");
+		}
+	}
 
-    FD_ZERO(&fd);
+	if (dir & LIBSSH2_SESSION_BLOCK_OUTBOUND) {
+		if (uwsgi.wait_write_hook(socket_fd, uwsgi.socket_timeout) < 0) {
+			uwsgi_error("waitsocket()/wait_write_hook()");
+		}
+	}
 
-    FD_SET(socket_fd, &fd);
-
-    /* now make sure we wait in the correct direction */
-    dir = libssh2_session_block_directions(session);
-
-
-    if (dir & LIBSSH2_SESSION_BLOCK_INBOUND)
-        readfd = &fd;
-
-    if (dir & LIBSSH2_SESSION_BLOCK_OUTBOUND)
-        writefd = &fd;
-
-    rc = select(socket_fd + 1, readfd, writefd, NULL, &timeout);
-
-    return rc;
+	return;
 }
 
-static int ssh_request_file(
-	const char *hostaddr,
-	uint32_t port,
-	const char *scppath,
-	void *file,
-	size_t *file_len) {
-
+static int init_ssh_session(char* remoteaddr, int *socket_fd, LIBSSH2_SESSION **session) {
 	int auth_pw = 1;
-	const char *username = "vagrant";
-	const char *password = "vagrant";
+	const char *username = USER;
+	const char *password = PASSWORD;
+
+	int sock = uwsgi_connect(remoteaddr, uwsgi.socket_timeout, 1);
+	if (sock < 0) {
+		uwsgi_error("init_ssh_session()/uwsgi_connect()");
+		return 1;
+	}
 
 	int rc = libssh2_init(0);
 	if (rc) {
-	    uwsgi_error("libssh2 initialization failed");
-	    exit(1);
+		uwsgi_error("init_ssh_session()/libssh2_init()");
+		goto shutdown;
 	}
 
-	struct sockaddr_in sin;
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(port);
-	if (inet_aton(hostaddr, &(sin.sin_addr)) != 1) {
-		uwsgi_error("error on inet_aton");
-		exit(1);
-	};
-
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock == -1) {
-		uwsgi_error("error on socket");
-		exit(1);
-	}
-
-	if (connect(sock, (struct sockaddr*)(&sin),
-	        sizeof(struct sockaddr_in)) != 0) {
-	    uwsgi_error("failed to connect!");
-	    exit(1);
-	}
-
-	/* Create a session instance */
-	LIBSSH2_SESSION *session = libssh2_session_init();
+	*session = libssh2_session_init();
 	if (!session) {
-		uwsgi_error("error on session");
-	    exit(1);
+		uwsgi_error("init_ssh_session()/libssh2_session_init()");
+		goto shutdown;
 	}
 
-	/* Since we have set non-blocking, tell libssh2 we are non-blocking */
-	libssh2_session_set_blocking(session, 0);
+	libssh2_session_set_blocking(*session, 0);
 
-	/* ... start it up. This will trade welcome banners, exchange keys,
-	 * and setup crypto, compression, and MAC layers
-	 */
-
-	while ((rc = libssh2_session_handshake(session, sock)) == LIBSSH2_ERROR_EAGAIN);
+	while ((rc = libssh2_session_handshake(*session, sock)) == LIBSSH2_ERROR_EAGAIN) {
+		waitsocket(sock, *session);
+	}
 	if (rc) {
-	    uwsgi_error("error on handshake");
-	    exit(1);
+		uwsgi_error("init_ssh_session()/libssh2_session_handshake()");
+		goto shutdown;
 	}
 
-	/* At this point we havn't yet authenticated.  The first thing to do
-	 * is check the hostkey's fingerprint against our known hosts.
-	 * Your app may have it hard coded, may go to a file, may present it to the
-	 * user, that's your call
-	 * TODO: Add fingerprint check!
-	 */
-	const char *fingerprint = libssh2_hostkey_hash(session, LIBSSH2_HOSTKEY_HASH_SHA1);
+	// TODO: Check remote fingerprint!
+	const char *fingerprint = libssh2_hostkey_hash(*session, LIBSSH2_HOSTKEY_HASH_SHA1);
 	if (!fingerprint) {
-		uwsgi_error("error on fingerprint");
-		exit(1);
+		uwsgi_error("init_ssh_session()/libssh2_hostkey_hash()");
+		goto shutdown;
 	}
-
-	// printf("Fingerprint: ");
-	// int i = 0;
-	// for (i = 0; i < 20; i++) {
-	//     printf("%02X ", (unsigned char)fingerprint[i]);
-	// }
-	// printf("\n");
 
 	if (auth_pw) {
-		while ((rc = libssh2_userauth_password(session, username, password)) == LIBSSH2_ERROR_EAGAIN);
-	    /* We could authenticate via password */
-	    if (rc) {
-	        uwsgi_error("Authentication by password failed.");
-	        goto shutdown;
-	    }
+		while ((rc = libssh2_userauth_password(*session, username, password)) == LIBSSH2_ERROR_EAGAIN) {
+			waitsocket(sock, *session);
+		}
+		if (rc) {
+			uwsgi_error("init_ssh_session()/libssh2_userauth_password()");
+			goto shutdown;
+		}
 	} else {
-	    /* Or by public key */
-	    // FIXME: keys path!
-	    while ((rc = libssh2_userauth_publickey_fromfile(
-		    		session, username,
-	                "/home/username/"
-	                ".ssh/id_rsa.pub",
-	                "/home/username/"
-	                ".ssh/id_rsa",
-                	password)
-	    ) == LIBSSH2_ERROR_EAGAIN);
-	    if (rc) {
-	        uwsgi_error("\tAuthentication by public key failed\n");
-	        goto shutdown;
-	    }
+		// FIXME: keys path!
+		while ((rc = libssh2_userauth_publickey_fromfile(
+					*session,
+					username,
+					"/home/username/"
+					".ssh/id_rsa.pub",
+					"/home/username/"
+					".ssh/id_rsa",
+					password)
+		) == LIBSSH2_ERROR_EAGAIN) {
+			waitsocket(sock, *session);
+		}
+		if (rc) {
+			uwsgi_error("init_ssh_session()/libssh2_userauth_publickey_fromfile()");
+			goto shutdown;
+		}
+	}
+
+	*socket_fd = sock;
+	return 0;
+
+shutdown:
+	close(sock);
+	return 1;
+}
+
+static int ssh_request_file(
+	struct wsgi_request *wsgi_req,
+	struct uwsgi_route *ur
+	) {
+
+	char *remoteaddr = ur->data;
+	char *scppath = ur->data2;
+
+	int sock = -1;
+
+	LIBSSH2_SESSION *session = NULL;
+	if (init_ssh_session(remoteaddr, &sock, &session)) {
+		uwsgi_error("ssh_request_file()/init_ssh_session()");
+		goto shutdown;
 	}
 
 	LIBSSH2_SFTP *sftp_session = NULL;
 	do {
-	    sftp_session = libssh2_sftp_init(session);
+		sftp_session = libssh2_sftp_init(session);
 
-	    if (!sftp_session) {
-	        if (libssh2_session_last_errno(session) == LIBSSH2_ERROR_EAGAIN) {
-	            uwsgi_error("non-blocking init\n");
-	            waitsocket(sock, session); /* now we wait */
-	        } else {
-	            uwsgi_error("Unable to init SFTP session\n");
-	            goto shutdown;
-	        }
-	    }
+		if (!sftp_session) {
+			if (libssh2_session_last_errno(session) == LIBSSH2_ERROR_EAGAIN) {
+				waitsocket(sock, session); /* now we wait */
+			} else {
+				uwsgi_error("ssh_request_file()/libssh2_sftp_init()");
+				goto shutdown;
+			}
+		}
 	} while (!sftp_session);
 
-	/* Request a file via SCP */
-	// LIBSSH2_SFTP *sftp_session = libssh2_sftp_init(session);
+	// Request file stats via SFTP
+	LIBSSH2_SFTP_ATTRIBUTES file_attrs;
+	int rc;
+	while ((rc = libssh2_sftp_stat(sftp_session, scppath, &file_attrs)) == LIBSSH2_ERROR_EAGAIN) {
+		waitsocket(sock, session);
+	}
+	if (rc < 0) {
+		uwsgi_error("ssh_request_file()/libssh2_sftp_stat()");
+		goto shutdown;
+	}
 
-	// if (!sftp_session) {
-	//     fprintf(stderr, "Unable to init SFTP session\n");
-	//     goto shutdown;
-	// }
+	// TODO: Error codes!
+	if (uwsgi_response_prepare_headers(wsgi_req, "200 OK", 6)) {
+		uwsgi_error("ssh_request_file()/uwsgi_response_prepare_headers()");
+		goto shutdown;
+	}
+
+	if (uwsgi_response_add_content_length(wsgi_req, file_attrs.filesize)) {
+		uwsgi_error("ssh_request_file()/uwsgi_response_add_content_length()");
+		goto shutdown;
+	}
+
+	if (uwsgi_response_add_last_modified(wsgi_req, file_attrs.mtime)) {
+		uwsgi_error("ssh_request_file()/uwsgi_response_add_last_modified()");
+		goto shutdown;
+	}
+
+	size_t mime_type_len = 0;
+	char *mime_type = uwsgi_get_mime_type(scppath, strlen(scppath), &mime_type_len);
+	if (mime_type) {
+		if (uwsgi_response_add_content_type(wsgi_req, mime_type, mime_type_len)) {
+			uwsgi_error("ssh_request_file()/uwsgi_response_add_content_type()");
+			goto shutdown;
+		}
+	}
 
 	/* Request a file via SFTP */
 	LIBSSH2_SFTP_HANDLE *sftp_handle = NULL;
 	do {
-	    sftp_handle = libssh2_sftp_open(sftp_session, scppath, LIBSSH2_FXF_READ, 0);
+		sftp_handle = libssh2_sftp_open(sftp_session, scppath, LIBSSH2_FXF_READ, 0);
 
-	    if (!sftp_handle) {
-	        if (libssh2_session_last_errno(session) != LIBSSH2_ERROR_EAGAIN) {
-	            uwsgi_error("Unable to open file with SFTP\n");
-	            goto shutdown;
-	        } else {
-	            uwsgi_log("non-blocking open\n");
-	            waitsocket(sock, session); /* now we wait */
-	        }
-	    }
+		if (!sftp_handle) {
+			if (libssh2_session_last_errno(session) != LIBSSH2_ERROR_EAGAIN) {
+				uwsgi_error("ssh_request_file()/libssh2_sftp_open()");
+				goto shutdown;
+			} else {
+				waitsocket(sock, session); /* now we wait */
+			}
+		}
 	} while (!sftp_handle);
 
-	size_t buffer_size = 1024;
-	void *buffer = uwsgi_malloc(buffer_size);
+	size_t buffer_size = BUFFER_SIZE;
+	void *buffer = alloca(buffer_size);
+	libssh2_uint64_t read_size = 0;
 
-	size_t f_len = 0;
-	void *f = NULL;  // let's start slow!
-	do {
+	while (read_size < file_attrs.filesize) {
 		rc = libssh2_sftp_read(sftp_handle, buffer, buffer_size);
-		uwsgi_log("%d\n", rc);
 
 		if (rc == LIBSSH2_ERROR_EAGAIN) {
-			// uwsgi_log("EAGAIN!");
 			waitsocket(sock, session);
-		} else if (rc <= 0) {
-			// TODO: error checking!
-			// We're done here!
-		    break;
+		} else if (rc < 0) {
+			uwsgi_error("ssh_request_file()/libssh2_sftp_read()");
+			break;
 		} else {
-		    f_len += rc;
-		    if ((f = realloc(f, f_len)) == NULL) {
-		    	uwsgi_error("error on realloc");
-		    	exit(1);
-		    }
-		    memcpy(f + (f_len - rc), buffer, rc);
+			read_size += rc;
+			if (uwsgi_response_write_body_do(wsgi_req, buffer, rc)) {
+				uwsgi_error("ssh_request_file()/uwsgi_response_write_body_do()");
+				break;
+			}
 		}
-	} while (1);
+	}
 
-	*file_len = f_len;
-	file = f;
+	while ((rc = libssh2_sftp_close(sftp_handle)) == LIBSSH2_ERROR_EAGAIN) {
+		waitsocket(sock, session);
+	};
+	if (rc < 0) {
+		uwsgi_error("ssh_request_file()/libssh2_sftp_close()");
+	}
 
-	uwsgi_log("%s, %d\n", f, f_len);
-
-	libssh2_sftp_close(sftp_handle);
-	libssh2_sftp_shutdown(sftp_session);
+	while ((rc = libssh2_sftp_shutdown(sftp_session)) == LIBSSH2_ERROR_EAGAIN) {
+		waitsocket(sock, session);
+	};
+	if (rc < 0) {
+		uwsgi_error("ssh_request_file()/libssh2_sftp_shutdown()");
+	}
 
 shutdown:
-	libssh2_session_disconnect(session, "Normal Shutdown, Thank you for playing");
+	while (libssh2_session_disconnect(session, "Normal Shutdown, thank you!") == LIBSSH2_ERROR_EAGAIN) {
+		waitsocket(sock, session);
+	}
 	libssh2_session_free(session);
-
+	close(sock);
 	libssh2_exit();
-	return 0;
-}
-
-static int request_handler(struct wsgi_request *request) {
-	if (uwsgi_parse_vars(request)) {
-		uwsgi_error("error parsing request vars");
-        exit(1);
-    }
-
-	char *s = "Request!";
-	size_t len = strlen(s) + 1;
-
-	uwsgi_response_prepare_headers(request, "200 OK", 6);
-	uwsgi_response_add_content_length(request, len);
-	uwsgi_response_add_content_type(request, "text/html", 9);
-	uwsgi_response_write_body_do(request, s, len);
-
 	return 0;
 }
 
 #ifdef UWSGI_ROUTING
 static int ssh_routing(struct wsgi_request *wsgi_req, struct uwsgi_route *ur) {
-	uwsgi_log("Data: %s\n", ur->data);
-	uwsgi_log("Data2: %s\n", ur->data2);
-	uwsgi_log("Data3: %s\n", ur->data3);
+	if (ssh_request_file(wsgi_req, ur)) {
+		uwsgi_error("error while requesting file");
+	}
 
-	int port = atoi(ur->data2);
-
-	void *file = NULL;
-	size_t size = 0;
-	int f = ssh_request_file(ur->data, port, ur->data3, file, &size);
-	uwsgi_log("%d", f);
-
-	// uwsgi_log("SSH Request!\n");
 	return 0;
 }
 
 static int ssh_router(struct uwsgi_route *ur, char *args) {
-        ur->func = ssh_routing;
-        ur->data = args;
-        ur->data_len = strlen(args);
+	ur->func = ssh_routing;
+	ur->data = args;
+	ur->data_len = strlen(args);
 
-        char *comma = strchr(ur->data, ',');
-        if (comma) {
-        	*comma = 0;
-        	ur->data_len = strlen(ur->data);
-        	ur->data2 = comma + 1;
-        	comma = strchr(ur->data2, ',');
-        	if (comma) {
-        		*comma = 0;
-        		ur->data3 = comma + 1;
-        		ur->data3_len = strlen(ur->data3);
-        	}
-        	ur->data2_len = strlen(ur->data2);
-        }
-        return 0;
+	char *comma = strchr(ur->data, ',');
+	if (comma) {
+		*comma = 0;
+		ur->data_len = strlen(ur->data);
+		ur->data2 = comma + 1;
+		ur->data2_len = strlen(ur->data2);
+	}
+	return 0;
 }
 
 static void register_ssh_router(void) {
+	// FIXME: Change me when you have proper options handling
+	uwsgi.build_mime_dict = 1;
 	uwsgi_register_router("ssh", ssh_router);
 }
 #endif
 
 struct uwsgi_plugin libssh2_plugin = {
-    .name = "libssh2",
+	.name = "libssh2",
 #ifdef UWSGI_ROUTING
-    .on_load = register_ssh_router,
+	.on_load = register_ssh_router,
 #endif
-    .request = request_handler,
+	// .request = request_handler,
 };
