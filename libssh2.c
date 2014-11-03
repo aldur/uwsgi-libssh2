4752223dd13b5d30d1270e8c7e82dfe1716eee4c
Aldur <adrianodl@hotmail.it>
2014-11-03 16:08:14 +0100

    fix indent

--- libssh2.c
+++ libssh2.c
@@ -6,93 +6,93 @@
 #if !defined(UWSGI_PLUGIN_API) || UWSGI_PLUGIN_API == 1
 // uWSGI < 2.1
 time_t uwsgi_parse_http_date(char *date, uint16_t len) {
-			struct tm hdtm;
+	struct tm hdtm;
 
-			if (len != 29 && date[3] != ',')
-					return 0;
+	if (len != 29 && date[3] != ',')
+		return 0;
 
-			hdtm.tm_mday = uwsgi_str2_num(date + 5);
+	hdtm.tm_mday = uwsgi_str2_num(date + 5);
 
-			switch (date[8]) {
-			case 'J':
-					if (date[9] == 'a') {
-							hdtm.tm_mon = 0;
-							break;
-					}
-
-					if (date[9] == 'u') {
-							if (date[10] == 'n') {
-									hdtm.tm_mon = 5;
-									break;
-							}
-
-							if (date[10] == 'l') {
-									hdtm.tm_mon = 6;
-									break;
-							}
-
-							return 0;
-					}
-
-					return 0;
-
-			case 'F':
-					hdtm.tm_mon = 1;
-					break;
-
-			case 'M':
-					if (date[9] != 'a')
-							return 0;
-
-					if (date[10] == 'r') {
-							hdtm.tm_mon = 2;
-							break;
-					}
-
-					if (date[10] == 'y') {
-							hdtm.tm_mon = 4;
-							break;
-					}
-
-					return 0;
-
-			case 'A':
-					if (date[10] == 'r') {
-							hdtm.tm_mon = 3;
-							break;
-					}
-					if (date[10] == 'g') {
-							hdtm.tm_mon = 7;
-							break;
-					}
-					return 0;
-
-			case 'S':
-					hdtm.tm_mon = 8;
-					break;
-
-			case 'O':
-					hdtm.tm_mon = 9;
-					break;
-
-			case 'N':
-					hdtm.tm_mon = 10;
+	switch (date[8]) {
+	case 'J':
+		if (date[9] == 'a') {
+			hdtm.tm_mon = 0;
 			break;
+		}
 
-			case 'D':
-					hdtm.tm_mon = 11;
-					break;
-			default:
-					return 0;
+		if (date[9] == 'u') {
+			if (date[10] == 'n') {
+				hdtm.tm_mon = 5;
+				break;
 			}
 
-			hdtm.tm_year = uwsgi_str4_num(date + 12) - 1900;
+			if (date[10] == 'l') {
+				hdtm.tm_mon = 6;
+				break;
+			}
 
-			hdtm.tm_hour = uwsgi_str2_num(date + 17);
-			hdtm.tm_min = uwsgi_str2_num(date + 20);
-			hdtm.tm_sec = uwsgi_str2_num(date + 23);
+			return 0;
+		}
 
-			return timegm(&hdtm);
+		return 0;
+
+	case 'F':
+		hdtm.tm_mon = 1;
+		break;
+
+	case 'M':
+		if (date[9] != 'a')
+			return 0;
+
+		if (date[10] == 'r') {
+			hdtm.tm_mon = 2;
+			break;
+		}
+
+		if (date[10] == 'y') {
+			hdtm.tm_mon = 4;
+			break;
+		}
+
+		return 0;
+
+	case 'A':
+		if (date[10] == 'r') {
+			hdtm.tm_mon = 3;
+			break;
+		}
+		if (date[10] == 'g') {
+			hdtm.tm_mon = 7;
+			break;
+		}
+		return 0;
+
+	case 'S':
+		hdtm.tm_mon = 8;
+		break;
+
+	case 'O':
+		hdtm.tm_mon = 9;
+		break;
+
+	case 'N':
+		hdtm.tm_mon = 10;
+	break;
+
+	case 'D':
+		hdtm.tm_mon = 11;
+		break;
+	default:
+		return 0;
+	}
+
+	hdtm.tm_year = uwsgi_str4_num(date + 12) - 1900;
+
+	hdtm.tm_hour = uwsgi_str2_num(date + 17);
+	hdtm.tm_min = uwsgi_str2_num(date + 20);
+	hdtm.tm_sec = uwsgi_str2_num(date + 23);
+
+	return timegm(&hdtm);
 }
 
 #endif
@@ -392,7 +392,7 @@ static int uwsgi_ssh_agent_auth(LIBSSH2_SESSION *session, int sock, char* userna
 	libssh2_agent_free(agent);
 	return 0;
 
-	shutdown:
+shutdown:
 
 	libssh2_agent_disconnect(agent);
 	libssh2_agent_free(agent);
@@ -738,7 +738,7 @@ static int uwsgi_ssh_request_file(
 		uwsgi_error("uwsgi_ssh_request_file()/libssh2_sftp_close()");
 	}
 
-	sftp_shutdown:
+sftp_shutdown:
 
 	while ((rc = libssh2_sftp_shutdown(sftp_session)) == LIBSSH2_ERROR_EAGAIN) {
 		uwsgi_ssh_waitsocket(sock, session);
@@ -747,7 +747,7 @@ static int uwsgi_ssh_request_file(
 		uwsgi_error("uwsgi_ssh_request_file()/libssh2_sftp_shutdown()");
 	}
 
-	shutdown:
+shutdown:
 
 	while (libssh2_session_disconnect(session, "Normal Shutdown, thank you!") == LIBSSH2_ERROR_EAGAIN) {
 		uwsgi_ssh_waitsocket(sock, session);
